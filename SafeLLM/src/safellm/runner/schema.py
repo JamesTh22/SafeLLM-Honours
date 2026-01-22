93180d3f01378c81f0e585c46cb40780a2f8a39a
James Thorley <j.thorley-2021@hull.ac.uk>
2026-01-22 14:10:22 +0000

    docs: comment line

--- SafeLLM/src/safellm/runner/schema.py
+++ SafeLLM/src/safellm/runner/schema.py
@@ -1,3 +1,4 @@
+# this file will create one json object per episode in a scenrio this is useful for training potential mitigation methods or detections 
 from __future__ import annotations
 
 from dataclasses import dataclass, asdict
