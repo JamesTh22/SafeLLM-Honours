596497760bdd543aa6e0d9da28747c1fb5ad7702
James Thorley <229487037+JamesTh22@users.noreply.github.com>
2026-01-27 17:06:37 +0000

    feat: logic for translation for inspect ai

--- SafeLLM/src/safellm/custombackend/inspect_model.py
+++ SafeLLM/src/safellm/custombackend/inspect_model.py
@@ -6,3 +6,24 @@ from inspect_ai.model import (
 )
 from .huggingface_nnsight import HuggingFaceNNSightBackend
 
+@model("nnsight")
+def backendNNsight(model_name: str, **model_kwargs: Any) -> Model:  # routes inspect ai to custome backend 
+    return Backend(model_name, **model_kwargs)
+
+class Backend(Model):
+    def __init__(self, model_name: str, **kwargs):
+        super().__init__(model_name, **kwargs)
+        self.backendargs = kwargs                      # store the auguments for the backend 
+        self.backend = None     
+    async def generate(
+            self, 
+        input: List[ChatMessage], 
+        tools: List[Any] = [], 
+        tool_choice: Any = None, 
+        config: Any = None
+    ) -> ModelOutput:
+        
+        if self.backend is None:
+            self.backend = HuggingFaceNNSightBackend(self.name, **self.backend_args)  # initisalise the backend only once
+
+          
